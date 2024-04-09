a98b4c66dc2e80ade56332a7157ea33b65873596
ummetarin <ummehomairatarin@gmail.com>
2024-04-09 17:09:06 +0600

    Finished country data showing in table

--- src/Components/Cities/Cities.tsx
+++ src/Components/Cities/Cities.tsx
@@ -1,29 +1,35 @@
+
+import React, { useEffect, useState } from "react";
 import CityesPart from "./CityesPart";
 
-
 const Cities = () => {
-    return (
-        <div>
-            <div>
-            <div className="overflow-x-auto ">
-            <table className="min-w-[90%] shadow-md  border mx-auto border-gray-100  my-6">
-                <thead>
-                    <tr className="bg-[#333333] text-white">
-                        <th className="py-3 px-6 text-left border-b">Name</th>
-                        <th className="py-3 px-6 text-left border-b">Age</th>
-                        <th className="py-3 px-6 text-left border-b">Gender</th>
-                        <th className="py-3 px-6  border-b text-end">Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                  <CityesPart></CityesPart>
-                </tbody>
+   const [data, setData] = useState([]);
+
+   useEffect(() => {
+      fetch('/Countrydata.json')
+      .then(res => res.json())
+      .then(allData => setData(allData))
+   }, []);
+
+   return (
+      <div>
+         <div className="overflow-x-auto">
+            <table className="min-w-[90%] shadow-md border mx-auto border-gray-100 my-6">
+               <thead>
+                  <tr className="bg-[#333333] text-white">
+                     <th className="py-3 px-6 text-left border-b">Country Name</th>
+                     <th className="py-3 px-6 text-left border-b">City Name</th>
+                     <th className="py-3 px-6 text-left border-b">Timezoon</th>
+                     <th className="py-3 px-6 border-b text-end">Country Code</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {data?.map(item => <tr className="hover:bg-gray-50 transition duration-300" key={item?.geoname_id}><CityesPart item={item} /></tr>)}
+               </tbody>
             </table>
-            </div>
-            
-                </div>
-        </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 export default Cities;
--- src/Components/Cities/CityesPart.tsx
+++ src/Components/Cities/CityesPart.tsx
@@ -1,14 +1,13 @@
+
 import React from "react";
 
-const CityesPart = () => {
+const CityesPart = ({ item }) => {
   return (
     <>
-      <tr className="hover:bg-gray-50 transition duration-300">
-        <td className="py-4 px-6 border-b">Shiyam </td>
-        <td className="py-4 px-6 border-b">19</td>
-        <td className="py-4 px-6 border-b">Male</td>
-        <td className="py-4 px-6 border-b text-end">Mirpur 15, Dhaka</td>
-      </tr>
+        <td className="py-4 px-6 border-b">{item.cou_name_en}</td>
+        <td className="py-4 px-6 border-b">{item.name}</td>
+        <td className="py-4 px-6 border-b">{item.timezone}</td>
+        <td className="py-4 px-6 border-b text-end">{item.country_code}</td>
     </>
   );
 };
